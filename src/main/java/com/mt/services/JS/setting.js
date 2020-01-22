6724ca97bfc35ad6f5ff3a8aef22ce9a34a9da88
dipak2dp <dipakparida700@gmail.com>
2020-01-22 15:58:03 +0530

    added

---
+++ src/main/java/com/mt/services/JS/setting.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	
+	
+});
+
+function showusersAndRoles(){
+	$("#stng-img").hide();
+	$("#userandroles").css("border-bottom","2px solid blue");
+	$("#users-roles").slideDown("slow");
+}
